80eba3a399148c134c22f02a496138f21efa2e8a
kapilpokhrel <kapilpokhrel100@gmail.com>
2024-06-17 08:50:35 +0545

    Added train and predict methods

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -1,6 +1,8 @@
 package network
 
 import (
+	"fmt"
+
 	"github.com/kapilpokhrel/goNN/pkg/layer"
 	"gonum.org/v1/gonum/mat"
 )
@@ -11,9 +13,35 @@ type Network struct {
 	LossPrime func(*mat.Dense, *mat.Dense) *mat.Dense
 }
 
-func (netowrk *Network) Predict(input *mat.Dense) {
-
+func (network *Network) Predict(input *mat.Dense) *mat.Dense {
+	result := input
+	for _, layer := range network.Layers {
+		var err error
+		result, err = layer.Forward(result)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+	}
+	return result
 }
-func (network *Network) Train(input []*mat.Dense, output []*mat.Dense, epoch int, rate float64) {
 
+func (network *Network) BackProp(out_grad *mat.Dense, rate float64) {
+	in_grad := out_grad
+	for _, layer := range network.Layers {
+		in_grad = layer.Backward(in_grad, rate)
+	}
+}
+
+func (network *Network) Train(inputs []*mat.Dense, outputs []*mat.Dense, epoch int, rate float64) {
+	for i := 0; i < epoch; i++ {
+		err := float64(0)
+		for j, input := range inputs {
+			result := network.Predict(input)
+			err += network.Loss(outputs[j], result)
+
+			out_grad := network.LossPrime(outputs[j], result)
+			network.BackProp(out_grad, rate)
+		}
+	}
 }
